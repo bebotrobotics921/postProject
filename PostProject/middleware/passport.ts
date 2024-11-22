0ff7b15dcccbe33db6273b7cdf7a9cfa418987f7
bebotrobotics921 <jessica.tc.woo@gmail.com>
2024-11-22 12:07:18 -0800

    Add files via upload

---
+++ PostProject/middleware/passport.ts
@@ -0,0 +1,40 @@
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import {
+  getUserByEmailIdAndPassword,
+  getUserById,
+} from "../controller/userController";
+
+// ⭐ TODO: Passport Types
+const localLogin = new LocalStrategy(
+  {
+    usernameField: "uname",
+    passwordField: "password",
+  },
+  async (uname: string, password: string, done: any)=> {
+    // Check if user exists in databse
+    const user = await getUserByEmailIdAndPassword(uname, password);
+    return user
+      ? done(null, user)
+      : done(null, false, {
+          message: "Your login details are not valid. Please try again.",
+        });
+  }
+);
+
+// ⭐ TODO: Passport Types
+passport.serializeUser(function (user: Express.User, done: (err: any, id?: number) => void) {
+  done(null, user.id);
+});
+
+// ⭐ TODO: Passport Types
+passport.deserializeUser(function (id: number, done: (err: any, user?: any | false) => any) {
+  const user = getUserById(id);
+  if (user) {
+    done(null, user);
+  } else {
+    done({ message: "User not found" }, null);
+  }
+});
+
+export default passport.use(localLogin);
---
+++ PostProject/routers/postRouters.ts
@@ -0,0 +1,108 @@
+import express from "express";
+const router = express.Router();
+import { ensureAuthenticated } from "../middleware/checkAuth";
+import { getUser, getPosts, getPost, addPost, editPost, deletePost, addComment } from "../fake-db";
+
+router.get("/", async (req, res) => {
+  const posts = await getPosts(20);
+  const user = await req.user;
+  res.render("posts", { posts, user });
+});
+
+router.get("/create", ensureAuthenticated, (req, res) => {
+  res.render("createPosts");
+});
+
+router.post("/create", ensureAuthenticated, async (req, res) => {
+  // ⭐ TODO
+  const { title, url, creator, description, subgroup } = req.body;
+  const newPost = addPost(title, url, creator, description, subgroup);
+  res.redirect(`/posts/show/${newPost.id}`);
+});
+
+router.get("/show/:postid", async (req, res) => {
+  // ⭐ TODO  
+  const postId = Number(req.params.postid);
+  const post = await getPost(postId);
+  const user = await req.user;
+  res.render("individualPost", {post, user});
+});
+
+router.get("/edit/:postid", ensureAuthenticated, async (req, res) => {
+  // ⭐ TODO
+  const postId = Number(req.params.postid);
+  const post = await getPost(postId);
+  
+  // Check if the post exists
+  if (!post) {
+    return res.status(404).render('error', { message: 'Post not found' });
+  }
+  // Check if the logged-in user is the creator of the post
+  if (post.creator !== getUser(postId)) {
+    return res.status(403).render('error', { message: 'Unauthorized to edit this post' });
+  }
+  //Render the edit form with the current post data
+
+  res.render("editPost", {post});
+});
+
+router.post("/edit/:postid", ensureAuthenticated, async (req, res) => {
+  // ⭐ TODO
+  const postId = Number(req.params.postid);
+  const { title, description } = req.body;
+  const postToEdit = await editPost(postId, {title, description});
+  res.redirect(`/edit/${postToEdit}`);
+});
+
+router.get("/deleteconfirm/:postid", ensureAuthenticated, async (req, res) => {
+  // ⭐ TODO
+  const postId = req.params.postid;
+  const post = getPosts(Number(postId));
+  //check if post exists
+  if(!post) {
+    return res.status(404).render('error', { message: 'Post not found'});
+  }
+  //Check if its an authenticated user
+  /*if (post.creator !== getUser(postId)) {
+    return res.status(403).render('error', { message: 'Unauthorized to edit this post' });
+  }*/
+  await deletePost(Number(postId));
+  res.render('deleteConfirm', {post});
+});
+
+router.post("/delete/:postid", ensureAuthenticated, async (req, res) => {
+  // ⭐ TODO
+  const postId = Number(req.params.postid);
+  const post = await getPost(postId);
+  // Check if the post exists
+  if (!post) {
+    return res.status(404).render('error', { message: 'Post not found' });
+  }
+  //Check if its an authenticated user
+  /*if (post.creator !== getUser(postId)) {
+    return res.status(403).render('error', { message: 'Unauthorized to edit this post' });
+  }*/
+  //Delete the post
+  await deletePost(postId);
+  // Redirect back to subgroup the post belonged to
+  res.redirect('/subs/show/${post.subId}');
+});
+
+router.post(
+  "/comment-create/:postid",
+  ensureAuthenticated,
+  async (req, res) => {
+    // ⭐ TODO
+    const postId = Number(req.params.postid);
+    const { description } = req.body;
+    if(!description){
+      return res.redirect(`/posts/show/${postId}`);
+    }
+
+    await addComment(postId, description, '');
+    res.redirect(`/posts/show/${postId}`);
+
+  }
+);
+
+export default router;
